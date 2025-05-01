e85619d849aec02c2de7cbb2c1131147d0b5c91e
Abdullah Mamun <abdullah.mamun.0110@gmail.com>
2025-05-01 23:07:47 +0600

    feat: add speciality API endpoints and ISpecialty type for managing specialties

---
+++ src/redux/api/speciality.ts
@@ -0,0 +1,51 @@
+import { IResponse } from "@/types";
+import { baseApi } from ".";
+
+import { ISpecialty } from "@/types/speciality";
+
+const specialityApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    createSpeciality: build.mutation<ISpecialty, FormData>({
+      query: (credentials) => ({
+        url: "/specialties",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+    specialites: build.query<IResponse<ISpecialty[]>, void>({
+      query: () => ({
+        url: "/specialties",
+        method: "GET",
+      }),
+      providesTags: ["specialties"],
+    }),
+    speciality: build.query<IResponse<ISpecialty>, string>({
+      query: (id) => ({
+        url: `/specialties/${id}`,
+        method: "GET",
+      }),
+    }),
+    updateSpeciality: build.mutation<IResponse<ISpecialty>, FormData>({
+      query: (credentials) => ({
+        url: "/specialties",
+        method: "PATCH",
+        body: credentials,
+      }),
+      invalidatesTags: ["specialties"],
+    }),
+    deleteSpeciality: build.mutation<IResponse<ISpecialty>, string>({
+      query: (id) => ({
+        url: `/doctors/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["specialties"],
+    }),
+  }),
+});
+
+export const {
+ useSpecialitesQuery,
+ useCreateSpecialityMutation,
+ useUpdateSpecialityMutation,
+ useDeleteSpecialityMutation
+} = specialityApi;
---
+++ src/types/speciality.ts
@@ -0,0 +1,9 @@
+
+
+export interface ISpecialty {
+    id: string;
+    name: string;
+    icon: string;
+    createdAt: string;
+    updatedAt: string;
+  }
