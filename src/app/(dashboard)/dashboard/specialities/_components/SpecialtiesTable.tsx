3499658bf109beeeb4b0071057ece9eb3f086cba
Abdullah Mamun <abdullah.mamun.0110@gmail.com>
2025-05-01 23:08:01 +0600

    feat: refactor SpecialtiesTable component for improved data handling and rendering

--- src/app/(dashboard)/dashboard/specialities/_components/SpecialtiesTable.tsx
+++ src/app/(dashboard)/dashboard/specialities/_components/SpecialtiesTable.tsx
@@ -1,61 +1,77 @@
-'use client';
+"use client";
 
-import { Layout, Button, Space, Typography, Table, Breadcrumb } from 'antd';
+import { useSpecialitesQuery } from "@/redux/api/speciality";
+import { ISpecialty } from "@/types/speciality";
+import { Button, Space, Table } from "antd";
 
-import { Heart, Brain, Bone, Activity, Bluetooth as Tooth } from 'lucide-react';
+import { Heart, Brain, Bone, Activity, Bluetooth as Tooth } from "lucide-react";
+import Image from "next/image";
 
-const {  Content } = Layout;
+// const specialtiesData = [
+//   { key: "SP001", specialty: "Urology", icon: <Heart size={24} /> },
+//   { key: "SP002", specialty: "Neurology", icon: <Brain size={24} /> },
+//   { key: "SP003", specialty: "Orthopedic", icon: <Bone size={24} /> },
+//   { key: "SP004", specialty: "Cardiologist", icon: <Activity size={24} /> },
+//   { key: "SP005", specialty: "Dentist", icon: <Tooth size={24} /> },
+// ];
 
-const { Title } = Typography;
-const specialtiesData = [
-    { key: 'SP001', specialty: 'Urology', icon: <Heart size={24} /> },
-    { key: 'SP002', specialty: 'Neurology', icon: <Brain size={24} /> },
-    { key: 'SP003', specialty: 'Orthopedic', icon: <Bone size={24} /> },
-    { key: 'SP004', specialty: 'Cardiologist', icon: <Activity size={24} /> },
-    { key: 'SP005', specialty: 'Dentist', icon: <Tooth size={24} /> },
-  ];
-
-  const columns = [
-    {
-      title: '#',
-      dataIndex: 'key',
-      key: 'key',
-    },
-    {
-      title: 'Specialities',
-      dataIndex: 'specialty',
-      key: 'specialty',
-      render: (text: string, record: any) => (
-        <Space>
-          {record.icon}
-          {text}
-        </Space>
-      ),
-    },
-    {
-      title: 'Actions',
-      key: 'actions',
-      render: () => (
-        <Space size="middle">
-          <Button type="primary" ghost size="small">Edit</Button>
-          <Button danger size="small">Delete</Button>
-        </Space>
-      ),
-    },
-  ];
+const columns = [
+  {
+    title: "S.N",
+    key: "slNo",
+    render: (_: any, __: ISpecialty, index: number) => <span>#{index + 1}</span>,
+  },
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+    render: (text: string) => <span>{text}</span>,
+  },
+  {
+    title: "Specialities",
+    dataIndex: "name",
+    key: "name",
+    render: (text: string, record: ISpecialty) => (
+      <Space>
+        <Image
+          src={record.icon}
+          alt="icon"
+          width={50}
+          height={50}
+          className="mr-2"
+        />
+        {text}
+      </Space>
+    ),
+  },
+  {
+    title: "Actions",
+    key: "actions",
+    render: () => (
+      <Space size="middle">
+        <Button type="primary" ghost size="small">
+          Edit
+        </Button>
+        <Button danger size="small">
+          Delete
+        </Button>
+      </Space>
+    ),
+  },
+];
 export default function SpecialtiesTable() {
+  const { data } = useSpecialitesQuery();
+  const specialtiesData = data?.data || [];
   return (
-
-    <Table 
-      columns={columns} 
+    <Table
+      columns={columns}
       dataSource={specialtiesData}
       pagination={{
         total: 5,
         pageSize: 10,
         showSizeChanger: true,
-        showTotal: (total) => `Showing 1 to ${total} of ${total} entries`
+        showTotal: (total) => `Showing 1 to ${total} of ${total} entries`,
       }}
     />
-  
-  )
+  );
 }
